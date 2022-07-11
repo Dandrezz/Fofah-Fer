9de91c4bbe622d691cb4c8f63fe6fb585697d745
Diego Portero <diegoaporterol@gmail.com>
2022-07-10 21:39:36 -0500

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import bobesponja from './bobesponja.gif'
+
+function App() {
+
+  return (
+    <div className="section">
+      <div className='central-section'>
+        <h1>
+          ¡Fer! 🤗
+        </h1>
+        <img src={bobesponja} alt="bobesponja" />
+      </div>
+    </div>
+  )
+}
+
+export default App
